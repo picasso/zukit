19b4d0e2c05732366ed44d3070a766036ea4749c
picasso <dmitry.rudakov@icloud.com>
2021-12-31 13:34:52 +0100

    added 'jquery-helpers' to global export for Settings Page

--- src/scripts/zukit.js
+++ src/scripts/zukit.js
@@ -1,6 +1,7 @@
 import * as render from './render.js';
 import * as fetch from './fetch.js';
 import * as utils from './utils.js';
+import * as jq from './jquery-helpers.js';
 import * as icons from './icons.js';
 import * as components from './components/index.js';
 import debug from './debug.js';
@@ -9,6 +10,7 @@ wp.zukit = {
     render,
     fetch,
     utils,
+    jq,
     icons,
     components,
     debug,
