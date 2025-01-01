6ed57315acc02ef8d90d7fbf20af67d7b261f0e1
picasso <dmitry.rudakov@icloud.com>
2025-01-01 15:04:52 +0100

    add prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+// See https://prettier.io/docs/en/options.html
+
+/** @type {import('prettier').Config} */
+module.exports = {
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+}
