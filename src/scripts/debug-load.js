42d2d8755c295a72074bd540f595a5cf6aeac98b
picasso <dmitry.rudakov@me.com>
2025-02-07 16:04:08 +0100

    make 'DevTools' always available from global 'window' scope

--- src/scripts/debug-load.js
+++ src/scripts/debug-load.js
@@ -1,23 +1,11 @@
-import { noop, transform } from 'lodash-es'
-
-import debug from './debug.js'
-// import * as process from 'process'
-
-const isDev = process.env.NODE_ENV === 'development'
+import debug, { isSimpleType, logNames, markers } from './debug.js'
 
 // NOTE: debug helpers ----------------------------------------------------------------------------]
 
 const dev = debug
-// copy of `DevTools` with `silent` loggers
-const devNone = transform(
-	dev,
-	(acc, _, key) => {
-		acc[key] = noop
-	},
-	{},
-)
 
 // make `DevTools` available from global scope
 if (typeof window !== 'undefined') {
-	window.dev = isDev ? dev : devNone
+	window.dev = dev
+	window.dev.more = { logNames, isSimpleType, markers }
 }
